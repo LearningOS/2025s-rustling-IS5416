3679384222c8d655fb73f76ce12e4f3e960af341
IS5416 <1766493305@qq.com>
2025-04-08 08:32:05 +0800

    done

--- exercises/algorithm/algorithm10.rs
+++ exercises/algorithm/algorithm10.rs
@@ -5,7 +5,6 @@
 
 use std::collections::{HashMap, HashSet};
 use std::fmt;
-
 #[derive(Debug, Clone)]
 pub struct NodeNotInGraph;
 impl fmt::Display for NodeNotInGraph {
@@ -13,112 +12,72 @@ impl fmt::Display for NodeNotInGraph {
         write!(f, "accessing a node that is not in the graph")
     }
 }
-
 pub struct UndirectedGraph {
     adjacency_table: HashMap<String, Vec<(String, i32)>>,
 }
-
 impl Graph for UndirectedGraph {
     fn new() -> UndirectedGraph {
         UndirectedGraph {
             adjacency_table: HashMap::new(),
         }
     }
-
     fn adjacency_table_mutable(&mut self) -> &mut HashMap<String, Vec<(String, i32)>> {
         &mut self.adjacency_table
     }
-
     fn adjacency_table(&self) -> &HashMap<String, Vec<(String, i32)>> {
         &self.adjacency_table
     }
-
-    fn add_node(&mut self, node: &str) -> bool {
-        if self.adjacency_table.contains_key(node) {
-            false
-        } else {
-            self.adjacency_table.insert(node.to_string(), Vec::new());
-            true
-        }
-    }
-
-    fn add_edge(&mut self, edge: (&str, &str, i32)) {
-        let (from, to, weight) = edge;
-
-        // 确保两个节点都存在
-        self.add_node(from);
-        self.add_node(to);
-
-        // 添加边到邻接表
-        self.adjacency_table
-            .get_mut(from)
-            .unwrap()
-            .push((to.to_string(), weight));
-        self.adjacency_table
-            .get_mut(to)
-            .unwrap()
-            .push((from.to_string(), weight));
-    }
 }
-
 pub trait Graph {
     fn new() -> Self;
     fn adjacency_table_mutable(&mut self) -> &mut HashMap<String, Vec<(String, i32)>>;
     fn adjacency_table(&self) -> &HashMap<String, Vec<(String, i32)>>;
-
     fn add_node(&mut self, node: &str) -> bool {
-        if self.adjacency_table().contains_key(node) {
+        let mut table = self.adjacency_table_mutable();
+        if table.contains_key(node) {
             false
         } else {
-            self.adjacency_table_mutable()
-                .insert(node.to_string(), Vec::new());
+            table.insert(String::from(node), Vec::new());
             true
         }
     }
+    fn add_edge(&mut self, edge: (&str, &str, i32)) {
+        self.adjacency_table_mutable()
+            .entry(String::from(edge.0))
+            .or_insert(Vec::new())
+            .push((String::from(edge.1), edge.2));
 
-    fn add_edge(&mut self, edge: (&str, &str, i32));
-
-    fn contains(&self, node: &str) -> bool {
-        self.adjacency_table().contains_key(node)
+        self.adjacency_table_mutable()
+            .entry(String::from(edge.1))
+            .or_insert(Vec::new())
+            .push((String::from(edge.0), edge.2));
     }
-
+    fn contains(&self, node: &str) -> bool {
+        self.adjacency_table().get(node).is_some()
+    }
     fn nodes(&self) -> HashSet<&String> {
         self.adjacency_table().keys().collect()
     }
-
     fn edges(&self) -> Vec<(&String, &String, i32)> {
         let mut edges = Vec::new();
-        let mut seen = HashSet::new();
-
-        for (from_node, neighbors) in self.adjacency_table() {
-            for (to_node, weight) in neighbors {
-                // 确保无向边只记录一次
-                let edge = (from_node, to_node);
-                let reverse_edge = (to_node, from_node);
-
-                if !seen.contains(&reverse_edge) {
-                    edges.push((from_node, to_node, *weight));
-                    seen.insert(edge);
-                }
+        for (from_node, from_node_neighbours) in self.adjacency_table() {
+            for (to_node, weight) in from_node_neighbours {
+                edges.push((from_node, to_node, *weight));
             }
         }
-
         edges
     }
 }
-
 #[cfg(test)]
 mod test_undirected_graph {
     use super::Graph;
     use super::UndirectedGraph;
-
     #[test]
     fn test_add_edge() {
         let mut graph = UndirectedGraph::new();
         graph.add_edge(("a", "b", 5));
         graph.add_edge(("b", "c", 10));
         graph.add_edge(("c", "a", 7));
-
         let expected_edges = [
             (&String::from("a"), &String::from("b"), 5),
             (&String::from("b"), &String::from("a"), 5),
@@ -127,9 +86,8 @@ mod test_undirected_graph {
             (&String::from("b"), &String::from("c"), 10),
             (&String::from("c"), &String::from("b"), 10),
         ];
-
         for edge in expected_edges.iter() {
-            assert!(graph.edges().contains(edge));
+            assert_eq!(graph.edges().contains(edge), true);
         }
     }
 }
